3100d21811742141ae12b35bd0896efb57715b0c
ShBogdan <Infocom911>
2015-08-31 18:21:50 +0300

    DBHelper add external base

--- main/src/main/java/com/bogdan/learner/DBHelper.java
+++ main/src/main/java/com/bogdan/learner/DBHelper.java
@@ -4,17 +4,23 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class DBHelper extends SQLiteOpenHelper {
-    private final String LOG_TAG = "::::DBHelper::::";
+    private static final String LOG_TAG = "::::DBHelper::::";
     private static DBHelper dbHelper;
+    private Context mContext;
 
     private static final String KEY_ROWID = "_id";
     private static final String KEY_ENG = "english";
@@ -23,6 +29,7 @@ public class DBHelper extends SQLiteOpenHelper {
     private static final String KEY_VOICE = "voice";
     private static final String KEY_DATE = "date";
 
+    private static String DB_PATH;
     private static final String DATABASE_NAME = "dictionary.sqlite";
     private static final String DATABASE_TABLE = "words";
     private static final int DATABASE_VERSION = 1;
@@ -39,11 +46,10 @@ public class DBHelper extends SQLiteOpenHelper {
     ContentValues contentValues;
     Cursor cursor;
 
-
     @Override
     public void onCreate(SQLiteDatabase db) {
+        Log.d(LOG_TAG, "onCreate");
         db.execSQL(DATABASE_CREATE);
-        Log.d(LOG_TAG, "DATABASE_CREATE");
     }
 
     @Override
@@ -52,16 +58,34 @@ public class DBHelper extends SQLiteOpenHelper {
 
     private DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        Log.d(LOG_TAG, "DBHelper");
         dbHelper = this;
+        this.mContext = context;
+        if(android.os.Build.VERSION.SDK_INT >= 4.2){
+            DB_PATH = mContext.getApplicationInfo().dataDir + "/databases/";
+        } else {
+            DB_PATH = mContext.getFilesDir() + mContext.getPackageName() + "/databases/";
+        }
+        if(checkDataBase()){
+            Log.d(LOG_TAG, "exist");
+        }else{
+            try {
+                copyDataBase(mContext);
+                Log.d(LOG_TAG, "DBHelper+copyDataBase try");
+            } catch (IOException e) {
+                Log.d(LOG_TAG, "DBHelper+copyDataBase catch");
+            }
+            Log.d(LOG_TAG, "DATABASE_CREATE");
+        }
         uploadDb = uploadDb();
     }
 
     public static DBHelper getDbHelper(Context context){
+        Log.d(LOG_TAG, "getDbHelper");
         if(dbHelper == null){
             dbHelper = new DBHelper(context);
         }
         return dbHelper;
-
     }
 
 
@@ -99,11 +123,11 @@ public class DBHelper extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("DELETE FROM " + DATABASE_TABLE + " WHERE " + KEY_ENG + " = " + "\"" + english + "\"");
     }
 
-
     /**
      * Загружает таблибу и возвращает Map. Ключ дата изучения значение массив строк(значения слова)
      */
     private TreeMap uploadDb() {
+        Log.d(LOG_TAG, "uploadDb");
         TreeMap<Integer, ArrayList<String[]>> wordsDb = new TreeMap<>();
         sqLiteDatabase = dbHelper.getReadableDatabase();
         cursor = sqLiteDatabase.query(DATABASE_TABLE, null, null, null, null, null, null);
@@ -125,7 +149,6 @@ public class DBHelper extends SQLiteOpenHelper {
         }
 
         Log.d(LOG_TAG, "words_size===============================================" + wordsDb.size());
-
         for (Map.Entry<Integer, ArrayList<String[]>> el : wordsDb.entrySet()) {
             ArrayList<String[]> al = el.getValue();
             Log.d(LOG_TAG, ("Kay::::::::::::::::::::::::::::::::::::::::::::::: " + el.getKey()));
@@ -133,9 +156,60 @@ public class DBHelper extends SQLiteOpenHelper {
                 Log.d(LOG_TAG, "array:::::::::::::::::::::::::::::::::::::::::: " + Arrays.asList(al.get(i)) + ", ");
             }
         }
-
         return wordsDb;
     }
+
+    private boolean checkDataBase(){
+        Log.d(LOG_TAG, "checkDataBase");
+         sqLiteDatabase = null;
+        try{
+            String myPath = DB_PATH + DATABASE_NAME;
+            sqLiteDatabase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+        }catch(SQLiteException e){
+            //база еще не существует
+        }
+        if(sqLiteDatabase != null){
+            sqLiteDatabase.close();
+        }
+        return sqLiteDatabase != null ? true : false;
+    }
+
+    private void copyDataBase(Context context) throws IOException {
+        Log.d(LOG_TAG, "copyDataBase");
+        sqLiteDatabase = dbHelper.getWritableDatabase();
+        //Открываем локальную БД как входящий поток
+        InputStream myInput = context.getAssets().open(DATABASE_NAME);
+        //Путь ко вновь созданной БД
+        String outFileName = DB_PATH + DATABASE_NAME;
+        //Открываем пустую базу данных как исходящий поток
+        OutputStream myOutput = new FileOutputStream(outFileName);
+
+        //перемещаем байты из входящего файла в исходящий
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer))>0){
+            myOutput.write(buffer, 0, length);
+            Log.d(LOG_TAG, "copyDataBase while" + length);
+        }
+
+        //закрываем потоки
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    /*Уже не нужен*/
     public void removeWordFromUploadDb(){
 //        for (Map.Entry<Integer, ArrayList<String[]>> el : uploadDb.entrySet()) {
 //            ArrayList<String[]> al = el.getValue();
@@ -152,9 +226,6 @@ public class DBHelper extends SQLiteOpenHelper {
         uploadDb = uploadDb();
 
     }
-
-
-    /*Уже не нужен*/
     public ArrayList listUnknownWords() {
         ArrayList<String> listUnknownWords = new ArrayList<>();
         sqLiteDatabase = this.getReadableDatabase();
