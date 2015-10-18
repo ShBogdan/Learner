d45ad051a593b529db3e6ecc8d6ee7a22968391c
ShBogdan <Infocom911>
2015-10-18 20:12:34 +0300

    ...

--- main/src/main/java/com/bogdan/learner/fragments/FrgListAllWord.java
+++ main/src/main/java/com/bogdan/learner/fragments/FrgListAllWord.java
@@ -1,50 +1,84 @@
 package com.bogdan.learner.fragments;
 
+import android.app.Fragment;
 import android.app.ListFragment;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bogdan.learner.DBHelper;
-import com.bogdan.learner.MainActivity;
 import com.bogdan.learner.R;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Map;
 
-public class FrgListAllWord extends ListFragment {
+public class FrgListAllWord extends Fragment {
     private final String LOG_TAG = "::::FrgListAllWord::::";
     private final String SETTINGS = "com.bogdan.learner.SETTINGS";
-    ArrayList<Word> data1;
+    ArrayList<Word> arrayList;
+    ListView listView;
     MyAdapter adapter;
     SharedPreferences sp;
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frg_list_all_words, null);
         sp = getActivity().getSharedPreferences(SETTINGS, Context.MODE_PRIVATE);
-        data1 = new ArrayList<>();
+        arrayList = new ArrayList<>();
 
         fillDate();
 
-        adapter = new MyAdapter(getActivity(), data1);
-        setListAdapter(adapter);
+        adapter = new MyAdapter(getActivity(), arrayList);
+        listView = (ListView) view.findViewById(R.id.lvMain);
+        listView.setAdapter(adapter);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        listView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+                Toast.makeText(getActivity(), "", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+
+            }
+        });
+
+        return view;
     }
 
-    class MyAdapter extends BaseAdapter implements CompoundButton.OnCheckedChangeListener{
+    class MyAdapter extends BaseAdapter{
         Context context;
         ArrayList<Word> object;
-        CheckBox cbRemove;
-        CheckBox cbReLearn;
         Word word;
 
         MyAdapter(Context context, ArrayList<Word> words){
@@ -71,61 +105,30 @@ public class FrgListAllWord extends ListFragment {
         public View getView(int position, View convertView, ViewGroup parent) {
             if (convertView == null) {
                 convertView = LayoutInflater.from(context).inflate(R.layout.frg_list_words, parent, false);
+
             }
             word = (Word)getItem(position);
 
             ((TextView) convertView.findViewById(R.id.englishWord)).setText(word.eng);
             ((TextView) convertView.findViewById(R.id.russianWord)).setText(word.rus);
 
-            cbRemove = (CheckBox) convertView.findViewById(R.id.cbRemove);
-            cbReLearn = (CheckBox) convertView.findViewById(R.id.cbReLearn);
-
-            cbRemove.setTag(position);
-            cbRemove.setChecked(word.boxRemove);
-            cbRemove.setOnCheckedChangeListener(this);
-            cbReLearn.setTag(position);
-            cbReLearn.setChecked(word.boxReLearn);
-            cbReLearn.setOnCheckedChangeListener(this);
 
             return convertView;
         }
 
-        @Override
-        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            if (buttonView.getId() == R.id.cbRemove) {
-                if (isChecked) {
-                    word.boxRemove = true;
-                    word.boxReLearn = false;
-                    cbReLearn.setEnabled(false);
-                } else {
-                    word.boxRemove = false;
-                    cbReLearn.setEnabled(true);
-                }
-            }
-            if (buttonView.getId() == R.id.cbReLearn) {
-                if (isChecked) {
-                    word.boxReLearn = true;
-                    word.boxRemove = false;
-                    cbRemove.setEnabled(false);
-                } else {
-                    word.boxReLearn = false;
-                    cbRemove.setEnabled(true);
-                }
-            }
-        }
     }
 
     class Word implements Comparable<Word>{
         String eng;
         String rus;
-        boolean boxRemove;
-        boolean boxReLearn;
+        boolean box;
 
-        public Word(String eng, String rus, boolean boxRemove, boolean boxReLearn) {
+
+        public Word(String eng, String rus, boolean box) {
             this.eng = eng;
             this.rus = rus;
-            this.boxRemove = boxRemove;
-            this.boxReLearn = boxReLearn;
+            this.box = box;
+
         }
         @Override
         public int compareTo(Word another) {
@@ -141,7 +144,7 @@ public class FrgListAllWord extends ListFragment {
         for (Map.Entry<Integer, ArrayList<String[]>> el : DBHelper.getDbHelper(getActivity()).uploadDb.entrySet()) {
             if (el.getKey() != 0 && el.getKey() != 1) {
                 for (String[] word : el.getValue()) {
-                    data1.add(new Word(word[0], word[1], false, false));
+                    arrayList.add(new Word(word[0], word[1], false));
                     Log.d(LOG_TAG, el.getKey().toString());
                     Log.d(LOG_TAG, word[0]);
                 }
@@ -149,31 +152,31 @@ public class FrgListAllWord extends ListFragment {
         }
 //        В сулчайнов порядке "random"
         if(setting.equals("random")) {
-            Collections.shuffle(data1);
+            Collections.shuffle(arrayList);
         }
 //        В алфавитном порядке "alphabet"
         if(setting.equals("alphabet")){
-            Collections.sort(data1);
+            Collections.sort(arrayList);
         }
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        ((MyAdapter) getListAdapter()).notifyDataSetChanged();
-    }
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        ((MyAdapter) getListAdapter()).notifyDataSetChanged();
+//    }
 
     @Override
     public void onPause() {
         super.onPause();
-        for (int i = 0; i < data1.size(); i++) {
+        for (int i = 0; i < arrayList.size(); i++) {
 //            удаляем слово из бащы
-            if (data1.get(i).boxRemove)
-                DBHelper.getDbHelper(getActivity()).removeWordFromDb(data1.get(i).eng);
+            if (arrayList.get(i).box)
+                DBHelper.getDbHelper(getActivity()).removeWordFromDb(arrayList.get(i).eng);
 //            учим слово заново
-            if(data1.get(i).boxReLearn){
-                DBHelper.getDbHelper(getActivity()).updateWordDate(MainActivity.toDayDate , data1.get(i).eng);
-            }
+//            if(arrayList.get(i).boxReLearn){
+//                DBHelper.getDbHelper(getActivity()).updateWordDate(MainActivity.toDayDate , arrayList.get(i).eng);
+//            }
         }
     }
 }
